869bd7a5977a4ddc38c8748e11807ca4de52d2a4
chenqp <13542427@qq.com>
2017-06-02 13:48:10 +0800

    first commit

---
+++ Cognitive.Vue/js/readme.js
@@ -0,0 +1,11 @@
+﻿/*
+* http://cn.vuejs.org/v2/guide/
+* http://vuejs.org/
+* https://github.com/vuejs/vue
+* http://www.cnblogs.com/rock-roll/p/5692891.html
+* http://www.cnblogs.com/w-wanglei/p/5851472.html
+
+Vue的主要特点就和它官网（http://cn.vuejs.org/）所介绍的那样：
+（1） 简洁 （2） 轻量 （3）快速 （4） 数据驱动 （5） 模块友好 （6） 组件化
+
+*/
